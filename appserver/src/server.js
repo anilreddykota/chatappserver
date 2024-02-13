db28521351f7ecb395983001c797cbfb62913a0d
anilreddykota <22b91a5469@srkrec.ac.in>
2024-02-14 00:29:29 +0530

    server changes for socketio server

--- appserver/src/server.js
+++ appserver/src/server.js
@@ -6,20 +6,21 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
-    allowRequest: (req, callback) => {
-      callback(null, true);
-    },
-    cors: {
-      origin: ['https://ichatwithyou.vercel.app',"http://localhost:3000"],
-      methods: ['GET', 'POST'],
-    }
-  });
-  
-const cors = require('cors'); 
+  allowRequest: (req, callback) => {
+    callback(null, true);
+  },
+  cors: {
+    origin: ['https://ichatwithyou.vercel.app', "http://localhost:3000"],
+    methods: ['GET', 'POST'],
+  }
+});
+
+const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 // Initialize Firebase Admin SDK
 const serviceAccount = require('./newkey.json');
+const { send } = require('process');
 firebaseAdmin.initializeApp({
   credential: firebaseAdmin.credential.cert(serviceAccount),
   storageBucket: 'chatappsocketanil.appspot.com'
@@ -96,125 +97,154 @@ app.post('/login', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-  app.get('/users', async (req, res) => {
-    try {
-      const usersSnapshot = await db.collection('users').get();
-      const users = [];
-      
-      usersSnapshot.forEach((doc) => {
-        users.push({
-          uid: doc.id,
-          ...doc.data(),
-        });
-      });
+app.get('/users', async (req, res) => {
+  try {
+    const usersSnapshot = await db.collection('users').get();
+    const users = [];
 
-      res.status(200).json(users);
-    } catch (error) {
-      console.error('Error getting users:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+    usersSnapshot.forEach((doc) => {
+      users.push({
+        uid: doc.id,
+        ...doc.data(),
+      });
+    });
+
+    res.status(200).json(users);
+  } catch (error) {
+    console.error('Error getting users:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 // Function to generate a unique conversation ID
 function generateConversationId(userId1, userId2) {
-    // Sort the user IDs to ensure consistency
-    const sortedUserIds = [userId1, userId2].sort();
+  // Sort the user IDs to ensure consistency
+  const sortedUserIds = [userId1, userId2].sort();
 
-    // Concatenate the sorted user IDs to form the conversation ID
-    return sortedUserIds.join('_');
+  // Concatenate the sorted user IDs to form the conversation ID
+  return sortedUserIds.join('_');
 }
-
+const onlineUsers = {};
+const typingUsers = {};
 
 io.on('connection', (socket) => {
-    console.log('A user connected:', socket.id);
+  console.log('A user connected:', socket.id);
+  // Handle 'join' event when a user joins the chat
+  socket.on('join', async ({ userId, reciverId }) => {
+    socket.join(userId);
+    onlineUsers[userId] = true; // Set user online
+    console.log(`User ${userId} joined the chat with ${reciverId}`);
+    io.emit('userStatus', { userId, isOnline: onlineUsers[reciverId] });
+    try {
+      // Generate a unique conversation ID based on user IDs
+      const conversationId = generateConversationId(userId, reciverId);
 
-    // Handle 'join' event when a user joins the chat
-    socket.on('join', async ({ userId, reciverId }) => {
-      socket.join(userId);
-      console.log(`User ${userId} joined the chat with ${reciverId}`);
-    
-      try {
-        // Generate a unique conversation ID based on user IDs
-        const conversationId = generateConversationId(userId, reciverId);
-    
-        const messagesRef = db.collection('messages').doc(conversationId).collection('messages');
-        const snapshot = await messagesRef.orderBy('timestamp', 'asc').get();
-    
-        const messages = [];
-        snapshot.forEach((doc) => {
-          messages.push({ id: doc.id, ...doc.data() });
-        });
-    
-        // Emit the 'previousMessages' event to the specific user who joined
-        io.to(userId).emit('previousMessages', { messages });
-      } catch (error) {
-        console.error('Error fetching previous messages:', error);
+      const messagesRef = db.collection('messages').doc(conversationId).collection('messages');
+      const snapshot = await messagesRef.orderBy('timestamp', 'asc').get();
+
+      const messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ id: doc.id, ...doc.data() });
+      });
+
+      // Emit the 'previousMessages' event to the specific user who joined
+      io.to(userId).emit('previousMessages', { messages });
+    } catch (error) {
+      console.error('Error fetching previous messages:', error);
+    }
+
+    // Send a welcome message to the specific user who joined
+    io.to(userId).emit('newMessage', { senderId: 'system', text: 'Welcome to the chat!' });
+  });
+  socket.on('typing', ({ senderId, receiverId, isTyping }) => {
+    // Update the typing status of the sender
+
+    typingUsers[senderId] = isTyping;
+
+    // Broadcast the 'typing' event to the recipient
+    io.to(receiverId).emit('typing', { userId: senderId, isTyping: typingUsers[receiverId] });
+
+
+    // Set a timeout to clear typing status after 3 seconds if not updated
+    if (isTyping) {
+      setTimeout(() => {
+        typingUsers[senderId] = false;
+        if (receiverId) {
+          io.to(receiverId).emit('typing', { userId: senderId, isTyping: false });
+        }
+      }, 3000);
+    }
+  });
+  socket.on('getMessages', async ({ userId, reciverId }) => {
+    try {
+      // Generate a unique conversation ID based on user IDs
+      const conversationId = generateConversationId(userId, reciverId);
+
+      const messagesRef = db.collection('messages').doc(conversationId).collection('messages');
+      const snapshot = await messagesRef.orderBy('timestamp', 'asc').get();
+console.log(messages);
+      const messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ id: doc.id, ...doc.data() });
+      });
+      // Emit the 'previousMessages' event to the specific user who requested it
+      io.to(userId).emit('previousMessages', { messages });
+    } catch (error) {
+      console.error('Error fetching previous messages:', error);
+    }
+  });
+  // Handle 'sendMessage' event when a user sends a message
+  socket.on('checkUserStatus', ({ userId }) => {
+    const isOnline = onlineUsers.hasOwnProperty(userId);
+    io.to(socket.id).emit('userStatus', { userId, isOnline });
+  });
+  socket.on('sendMessage', async (data) => {
+    const { senderId, receiverId, text } = data;
+
+    try {
+      // Generate a unique conversation ID based on user IDs
+      const conversationId = generateConversationId(senderId, receiverId);
+
+      const messagesRef = db.collection('messages').doc(conversationId).collection('messages');
+      const timestamp = new Date();
+
+      // Save the message to Firestore with the unique conversation ID
+      await messagesRef.add({
+        senderId,
+        receiverId,
+        text,
+        timestamp,
+      });
+
+      // Emit the message to the sender
+      if (senderId !== receiverId) {
+        io.to(senderId).emit('newMessage', { senderId, receiverId, text, timestamp, conversationId });
       }
-    
-      // Send a welcome message to the specific user who joined
-      io.to(userId).emit('newMessage', { senderId: 'system', text: 'Welcome to the chat!' });
-    });
-        socket.on('getMessages', async ({ userId, reciverId }) => {
-        try {
-          // Generate a unique conversation ID based on user IDs
-          const conversationId = generateConversationId(userId, reciverId);
-    
-          const messagesRef = db.collection('messages').doc(conversationId).collection('messages');
-          const snapshot = await messagesRef.orderBy('timestamp', 'asc').get();
-    
-          const messages = [];
-          snapshot.forEach((doc) => {
-            messages.push({ id: doc.id, ...doc.data() });
-          });
-    
-          // Emit the 'previousMessages' event to the specific user who requested it
-          io.to(userId).emit('previousMessages', { messages });
-        } catch (error) {
-          console.error('Error fetching previous messages:', error);
-        }
-         });
-    // Handle 'sendMessage' event when a user sends a message
-        socket.on('sendMessage', async(data) => {
-        const { senderId, receiverId, text } = data;
 
-        try {
-            // Generate a unique conversation ID based on user IDs
-            const conversationId = generateConversationId(senderId, receiverId);
+      // Emit the message to the receiver
+      io.to(receiverId).emit('newMessage', { senderId, receiverId, text, timestamp, conversationId });
 
-            const messagesRef = db.collection('messages').doc(conversationId).collection('messages');
-            const timestamp = new Date();
+      // console.log('Message sent successfully');
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
+  });
 
-            // Save the message to Firestore with the unique conversation ID
-            await messagesRef.add({
-                senderId,
-                receiverId,
-                text,
-                timestamp,
-            });
-
-            // Emit the message to the sender
-            io.to(senderId).emit('newMessage', { senderId, receiverId, text, timestamp, conversationId });
-
-            // Emit the message to the receiver
-            io.to(receiverId).emit('newMessage', { senderId, receiverId, text, timestamp, conversationId });
-
-            // console.log('Message sent successfully');
-        } catch (error) {
-            console.error('Error sending message:', error);
-        }
-    });
-
-    // Disconnect event
-    socket.on('disconnect', () => {
-        console.log('User disconnected:', socket.id);
-    });
+  // Disconnect event
+  socket.on('disconnect', () => {
+    const disconnectedUser = Object.keys(onlineUsers).find((userId) => onlineUsers[userId] === socket.id);
+    if (disconnectedUser) {
+      delete onlineUsers[disconnectedUser];
+      io.emit('userStatus', { userId: disconnectedUser, isOnline: false });
+      console.log('User disconnected:', socket.id);
+    }
+  });
 });
-
 const PORT = 5000;
 
 server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 app.listen(7001, () => {
-    console.log(`Server is running on port 7001`);
+  console.log(`Server is running on port 7001`);
 });
